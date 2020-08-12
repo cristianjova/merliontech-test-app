4d1114de227f0e3421506535eb31d7706a354621
Cristian Jovanovich <cristian.jova149@gmail.com>
2020-08-12 17:03:36 -0300

    Initial version of Test generated by JHipster-6.10.1

---
+++ src/main/java/merliontechs/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,11 @@
+package merliontechs.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+
+}
---
+++ src/main/java/merliontechs/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package merliontechs.web.rest;
